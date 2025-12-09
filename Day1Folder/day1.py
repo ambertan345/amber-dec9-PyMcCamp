3282cc649e640865583f36bc3d231a737644c5fa
ambertan345 <ambertan2011@yahoo.com.sg>
2025-12-09 06:40:47 +0000

    Dec 9, 2025, 6:40 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,15 +1,18 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day1")
+# print("hello from day1")
 
 ########################################################################
 # Task 1:
-
+# print("Hello World!")
 
 
 ########################################################################
 # Task 2:
-
+print("my name is amber")
+print("im 14")
+print("i go to chij st.nicholas girls school secondary")
+print("if i has 2000 as a reward, i would buy food & lego and a ps5")
 
 
 ########################################################################
