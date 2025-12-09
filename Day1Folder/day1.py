7a1dcae5b12ec18e70f02bc1cdba7666d19731d6
ambertan345 <ambertan2011@yahoo.com.sg>
2025-12-09 06:42:58 +0000

    Dec 9, 2025, 6:42 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -15,6 +15,7 @@ print("i go to chij st.nicholas girls school secondary")
 print("if i has 2000 as a reward, i would buy food & lego and a ps5 then invest the rest in bitcoin")
 
 
+
 ########################################################################
 # Task 3:
 
