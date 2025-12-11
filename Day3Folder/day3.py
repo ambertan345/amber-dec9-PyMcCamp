f5a4fe041578fecbb0133208040f2f9fff885b0a
ambertan345 <ambertan2011@yahoo.com.sg>
2025-12-11 06:53:58 +0000

    Dec 11, 2025, 6:53 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -16,7 +16,7 @@ print("hello from day3")
 # Task 2:
 num1 = input(str("What is your first number? "))
 num2 = input(str("What is your second number? "))
-sum = int(num1) +  
+sum = int(num1) + int(num2) 
 print(num1 + " + " + num2 + " = " + sum)
 
 
